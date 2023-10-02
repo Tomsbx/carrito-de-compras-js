d5f9b3b6f3149e4e4494ac1f173a4cadf2ffdc2c
Tomas <tomassilvera99@gmail.com>
2023-10-02 13:12:00 -0300

    e-commerce con JS

---
+++ js/data.js
@@ -0,0 +1,30 @@
+let shopItemsData = [
+  {
+    id: "jfhgbvnscs",
+    name: "Levi Funko Pop",
+    price: 45,
+    desc: "El genio de las espadas.",
+    img: "../images/levi-funkopop.webp",
+  },
+  {
+    id: "ioytrhndcv",
+    name: "Dr. Strange Funko Pop",
+    price: 45,
+    desc: "Doctor magias de Marvel.",
+    img: "../images/drstrange-funkopop.webp",
+  },
+  {
+    id: "wuefbncxbsn",
+    name: "Iron Man Funko Pop",
+    price: 55,
+    desc: "Traje a bases de raices.",
+    img: "../images/ironman-funkopop.webp",
+  },
+  {
+    id: "thyfhcbcv",
+    name: "Loki Funko Pop",
+    price: 300,
+    desc: "El mentiras.",
+    img: "../images/loki-funkopop.webp",
+  },
+];
